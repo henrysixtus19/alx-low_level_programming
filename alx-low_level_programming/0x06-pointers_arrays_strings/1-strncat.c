ce8961d4ce27aa9ec6b738452b9657cc2d7ed7f9
henrysixtus19 <sixtus91@gmail.com>
2023-08-23 11:46:40 -0700

    0-strcat.c

---
+++ alx-low_level_programming/0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * strncat : contenates two strings
+ * @dest : destination string
+ * @src : source string
+ * return : dest
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+
+}
