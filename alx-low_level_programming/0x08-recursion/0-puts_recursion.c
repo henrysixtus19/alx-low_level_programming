c7692df9bdfd97a7a5d4d4400ceb622e5e7411be
henrysixtus19 <sixtus91@gmail.com>
2023-08-30 09:21:36 -0700

    ./0-puts_recursion

--- alx-low_level_programming/0x08-recursion/0-puts_recursion.c
+++ alx-low_level_programming/0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints's a string followed by a new line
- * @s: string to be printed
- *
- * Return: void
+ * _puts_recursion - prints string
+ * @s: string to print
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar(s[0]);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n');
 }
